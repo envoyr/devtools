639cf61909c195eadd5a37acc0640062651128f4
envoyr <hello@envoyr.com>
2021-07-28 23:11:53 +0200

    Update add.sh

--- bin/user/add.sh
+++ bin/user/add.sh
@@ -2,11 +2,18 @@
 
 user=$3
 webserver="www-data"
+storage_path="/home/$user/www"
 
 # Add new user to the system
 sudo adduser --gecos GECOS $user
 
 # Add nginx user to new created user
 sudo usermod -a -G $user $webserver
+
+# Create new directory for user
+sudo mkdir $storage_path
+
+# Set permissions for storahe path
+devtools permissions set $user $storage_path
 
 echo "User created!"

b5c9e4b2ae5fa8794cbda84cce100f2ad99b66cd
envoyr <hello@envoyr.com>
2021-07-29 08:19:59 +0200

    Update set.sh

--- bin/permissions/set.sh
+++ bin/permissions/set.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
-username=$3
+user=$3
 storage_path=$4
 
+# Check if user name is set
+if [ -z "$user" ]; then
+  echo "Error: User not entered!"
+  exit 1
+fi
+
+# Check if storage path is set
+if [ -z "$storage_path" ]; then
+  echo "Error: Storage path not entered!"
+  exit 1
+fi
+
 # All the files should be owned by the website user and the website user's group
-sudo chown -R $username:$username $storage_path
+sudo chown -R $user:$user $storage_path
 
 # The following general chmod setup will allow for any website to function properly
 # In octal notation, this results in 0750 chmod for all directories and 0640 for all files

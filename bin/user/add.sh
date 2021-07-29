e74c3401351896daac53084550f7404edaa0c3eb
envoyr <hello@envoyr.com>
2021-07-29 08:27:29 +0200

    Update add.sh

--- bin/user/add.sh
+++ bin/user/add.sh
@@ -4,6 +4,18 @@ user=$3
 webserver="www-data"
 storage_path="/home/$user/www"
 
+# Check if user name is set
+if [ -z "$user" ]; then
+  echo "Error: User not entered!"
+  exit 1
+fi
+
+# Check if user already exist
+if id "$user" &>/dev/null; then
+  echo "Error: User already exist!"
+  exit 1
+else
+
 # Add new user to the system
 sudo adduser --gecos GECOS $user
 

8c6dc9f1c81ef10873d5248f8a1ffcdfc6ca15e5
envoyr <hello@envoyr.com>
2021-09-15 19:00:16 +0200

    Update certbot.sh

--- bin/core/install/certbot.sh
+++ bin/core/install/certbot.sh
@@ -3,4 +3,4 @@
 sudo apt-get install -y snapd
 sudo snap install core; sudo snap refresh core
 sudo snap install --classic certbot
-sudo ln -s /snap/bin/certbot /usr/bin/certbot
+sudo ln -sf /snap/bin/certbot /usr/bin/certbot

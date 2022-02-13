9f93d3d3f9585521ac21a428567773d81e747f19
envoyr <hello@envoyr.com>
2022-02-13 22:26:05 +0100

    Update command

--- bin/domain/move.sh
+++ bin/domain/move.sh
@@ -6,12 +6,23 @@ set -e
 
 # Set config variables
 export domain=$3
-export from=$4
+export from=${4%/}
 export to="$DEVTOOLS_WEB_DIRECTORY/$domain"
 
-# Move files to new location
-echo "Moving files from $from $to"
-mv "$from/" "$to/"
+# Check if domain name is set
+if [ -z "$domain" ]; then
+  echo "Error: Domain not entered!"
+  exit 1
+fi
+
+# Check if source path is set and move files
+if id "$from" &>/dev/null; then
+  echo "Moving files from $from $to"
+  mv "$from/" "$to/"
+else
+  echo "Source path not exist!"
+  exit 1
+fi
 
 # Copy new config file and enable in nginx
 sed -i "s|$from|$to|g" "/etc/nginx/sites-available/$domain"

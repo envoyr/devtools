b3af1c10f3343a3a90614e82780ffe56e49123b6
envoyr <hello@envoyr.com>
2022-02-13 22:10:12 +0100

    Update command

--- bin/domain/move.sh
+++ bin/domain/move.sh
@@ -11,7 +11,7 @@ export to="$DEVTOOLS_WEB_DIRECTORY/$domain"
 
 # Move files to new location
 echo "Moving files from $from $to"
-mv "$from" "$to"
+mv "$from/" "$to/"
 
 # Copy new config file and enable in nginx
 sed -i "s/$from/$to/" "/etc/nginx/sites-available/$domain"
